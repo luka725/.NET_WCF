e46c9d62310df4be904cdd4e508116f45900489d
luka <luka.sheylashvili@gmail.com>
2024-03-13 20:03:12 +0400

    pushing first part

---
+++ DatabaseService/DataProvider.svc.cs
@@ -0,0 +1,26 @@
+﻿using ClassLibraryDatabase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace DatabaseService
+{
+    public class DataProvider : IDataProvider
+    {
+        public Customer GetCustomer()
+        {
+            using (var dbContext = new DatabaseModel())
+            {
+                int customerIdToRetrieve = 1;
+
+                var customer = dbContext.Customers
+                                        .FirstOrDefault(c => c.CustomerID == customerIdToRetrieve);
+
+                return customer;
+            }
+        }
+    }
+}
